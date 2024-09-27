6fed78d905ff915ed7434b52504361b371d89757
rnybby <153337911+rnybby@users.noreply.github.com>
2024-09-27 02:57:54 -0400

    Add files via upload

---
+++ python-ds-practice/23_list_check/list_check.py
@@ -0,0 +1,13 @@
+def list_check(lst):
+    """Are all items in lst a list?
+
+        >>> list_check([[1], [2, 3]])
+        True
+
+        >>> list_check([[1], "nope"])
+        False
+    """
+    for lsts in lst:
+        if not isinstance(lsts, list):
+            return False
+    return True
